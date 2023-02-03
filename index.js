3cd40e96d22b1bf9cbe1c2e591e20ac49a716ee6
Vasu Garg <96354306+VasuGargDev@users.noreply.github.com>
2023-02-03 18:04:24 +0000

    1.2

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-// console.log("Vasu Garg");
 var readlineSync = require('readline-sync');
+var score = 0;
 
-var userName = readlineSync.question("Your name please ");
 
-var welcomeMessage = "Welcome " + userName;
-console.log(welcomeMessage);
+var questionOne = "Are you under 25?"
+
+var userAnswer = readlineSync.question(questionOne);
+
+console.log("You entered " + userAgeInput);
+
+if(userAgeInput === "yes") {
+  console.log("You are right.")
+  score = score + 1;
+  console.log("Score is:" + score);
+} 
+else {
+  console.log("You are wrong.")
+  score = score - 1;
+  console.log("Score is:" + score);
+}
+
+
+var questionTwo = "Are you from Delhi?"
+
+var userAgeInput = readlineSync.question(questionTwo );
+
+console.log("You entered " + userAgeInput);
+
+if(userAgeInput === "yes") {
+  console.log("You are right.")
+  score = score + 1;
+  console.log("Score is:" + score);
+} 
+else {
+  console.log("You are wrong.")
+  score = score - 1;
+  console.log("Score is:" + score);
+}
